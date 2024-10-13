0ba12eeffd880b4bcee03bc6fcc059b618af35d3
hsahmad112 <hsahmad112@gmail.com>
2024-10-13 15:50:33 +0200

    seperated the big if statement in ValidateRequest() into seperate Validators, currently called inside ValidateRequest.

--- Server/Response.cs
+++ Server/Response.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Xunit.Sdk;
 
 
-    public class Response
+public class Response
     {
 
-        public string Status { get; set; } 
-        public string Body { get; set; }
+        public string? Status { get; set; } 
+        public string? Body { get; set; }
 
 
         public Response(string status, string body) // constructors
@@ -19,11 +20,15 @@ using System.Threading.Tasks;
             Body = body;
 
         }
+    public Response() // constructors
+    {
 
-  
+
+    }
+
     public void AddOrAppendToStatus(string statusMessage) //Method that adds or appends to Status 
         {
-            if (Status == "")
+            if (Status == "" || Status == null)
             {
                 Status = statusMessage;
             }
@@ -33,11 +38,38 @@ using System.Threading.Tasks;
             }
         }
 
-    public void clearStatus(string statusMessage)
+    public void AddorAppendToBody(string bodyMessage)
+    {
+        if (Body == "")
+        {
+            Body = bodyMessage;
+        }
+        else
+        {
+            Body = Body + " , " + bodyMessage;
+        }
+    }
+
+
+    public Response AddResponse(Response oldResponse, Response newResponse)
+    {
+        string delimiter = " , ";
+        Response combinedResponse = new Response();
+        combinedResponse.Status = oldResponse.Status += delimiter  += newResponse.Status;
+        combinedResponse.Body = oldResponse.Body += delimiter += newResponse.Body;
+
+        return combinedResponse;
+    }
+
+    public void ClearStatus()
     {
         Status = "";
     }
 
 
+    public void ClearBody()
+    {
+        Body = "";
+    }
 }
 
--- Server/Server.cs
+++ Server/Server.cs
@@ -5,7 +5,11 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text.Json;
 using Assignment3TestSuite;
-using System.Net.Http.Headers;
+using System.Reflection;
+using Xunit.Sdk;
+using System.IO;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
+
 
 
 public class Server
@@ -75,20 +79,15 @@ public class Server
 
 
 
-    public void PrintCategories()
+    public List<Category> PrintCategories()
     {
-
         List<Category> categories = new List<Category>()
     {
         new Category(1,"Beverages"),
         new Category(2, "Condiments"),
         new Category(3, "Confections")
     };
-        foreach (Category category in categories)
-        {
-            Console.WriteLine($"cid: {category.cid}, name: {category.name}");
-            Console.WriteLine(JsonSerializer.Serialize<Category>(category));
-        }
+        return categories;
 
     }
 
@@ -102,92 +101,225 @@ public class Server
 
         var stream = client.GetStream();
         var jsonMessage = ReadFromStream(stream);
-        Console.WriteLine(jsonMessage);
+        
         Request? clientRequest = clientRequest = JsonSerializer.Deserialize<Request>(jsonMessage, options);
         // Console.WriteLine("clientRequest body" + clientRequest.Body);  Proof of concept - getting body here
         return clientRequest;
     }
 
 
+    
+    
+
+
     void ValidateRequest(TcpClient client, Request req)
     {
-        Response response = new Response("", "");
+        Response response = new Response();
         var stream = client.GetStream();
         string[] validMethods = ["create", "read", "update", "delete", "echo"];
         string[] validMethodsForBody = ["create", "update", "echo"];
 
-        string partialPath = "API/categories";
-
-        Console.WriteLine(req.Body);
-        //if any element in request missing
-        if (req.Body == null && req.Date == null && req.Path == null && req.Method == null)
-        {
-            ;
-            if (req.Body == null)
-            {
-                response.AddOrAppendToStatus("missing body");
-            }
-            if (req.Date == null)
-            {
-                response.AddOrAppendToStatus("missing date");
-            }
-            if (req.Method == null)
-            {
-                response.AddOrAppendToStatus("missing method");
-            }
-            if (req.Path == null)
-            {
-                response.AddOrAppendToStatus("missing resource");
-
-            }
-
-            //WriteToStream(stream, ToJson(response)); //Dont know why this does not work
-            WriteToStream(stream, JsonSerializer.Serialize(response)); //temp solution Writes to stream
-        }
 
 
-        foreach (var method in validMethods)
-        {
-            if (req.Method != method)
-            {
-                response.AddOrAppendToStatus("illegal method");
+        EchoChecker(req, client);
 
-                if (req.Body == null && req.Path == null)
-                {
-                    response.AddOrAppendToStatus("missing resource");
-
-                }
-
-                if (req.Body == null)
-                {
-                    response.AddOrAppendToStatus("missing body");
-
-                }
-
-                if (req.Date.Contains("/"))
-                {
-                    response.clearStatus("");
-                    response.AddOrAppendToStatus("illegal date");
-                    WriteToStream(stream, JsonSerializer.Serialize(response));
-
-                }
-
-                else
-                {
-
-                    WriteToStream(stream, JsonSerializer.Serialize(response));
-                }
+        response = response.AddResponse(ValidateBody(req), DateValidator(req));
+        response = response.AddResponse(response, ValidateMethod(req));
+        response = response.AddResponse(response, ValidatePath(req));
+        WriteToStream(stream, JsonSerializer.Serialize(response));
 
 
-            }
-
-
-        }
     }
 
-     
 
-        public static string ToJson(Response response)
+
+    /*******************************************METHODS****************************************/
+    /******************************************************************************************/
+    /******************************************************************************************/
+    /******************************************************************************************/
+
+    void EchoChecker(Request request, TcpClient client) 
+    {
+        Response response = new Response();
+        
+        var stream = client.GetStream();
+
+        if (request.Method == "echo" && !String.IsNullOrEmpty(request.Body))
+        {
+            response.ClearBody();
+            response.ClearStatus();
+            response.AddorAppendToBody(request.Body);
+            WriteToStream(stream, JsonSerializer.Serialize(response));
+            return;
+        }
+        else { return; }
+
+    } 
+    Boolean EchoChecker(Request request)
+    {
+        
+        if(request.Method == "echo")
+        {
+            
+            return true;
+        }
+
+        return false;
+    }
+
+
+    Response DateValidator(Request request)
+    {
+        Response response = new Response();
+
+        if (request.Date == null)
+        {
+            response.AddOrAppendToStatus("missing date");
+            return response;
+        }
+
+        if (request.Date.Contains("/")) //uh not ideal but... works? if time (haha) allows do something smarter
+        {
+            response.AddOrAppendToStatus("illegal date");
+            return response;
+        }
+
+        return response;
+    }
+
+    Response ValidateMethod(Request request)
+    {
+        Response response = new Response();
+        string[] validMethods = ["create", "read", "update", "delete", "echo"];
+        string[] validMethodsForBody = ["create", "update", "echo"];
+
+
+        if (request.Method == null)
+        {
+            Console.WriteLine("Method not found, thus missing");
+            response.AddOrAppendToStatus("missing method");
+            return response;
+        }
+
+
+        foreach (var method in validMethods) {
+            if (!method.Any(request.Method.Contains))
+            {
+                goto notfound;
+            }
+           
+        }
+        notfound:
+        Console.WriteLine("Method does not exist thus illegal");
+        response.AddOrAppendToStatus("illegal method");
+        
+        return response;
+    }
+
+    Response ValidateBody(Request request)
+    {
+        Response response = new Response();
+
+        if (String.IsNullOrEmpty(request.Body)) {
+
+            Console.WriteLine("Body contains nothing");
+            response.AddOrAppendToStatus("missing body");
+            return response;
+        }
+        else { 
+        try
+        {
+            JsonDocument.Parse(request.Body);
+        }
+        catch
+        {
+            response.Status = "illegal body";
+
+            return response;
+            }
+        }
+
+        return response;
+    }
+
+     Response ValidatePath(Request request) {
+
+        Response response = new Response();
+
+        List<Category> categories = new List<Category>()
+        {
+        new Category(1,"Beverages"),
+        new Category(2, "Condiments"),
+        new Category(3, "Confections")
+        };
+        const string partialPath = "/api/"; //We hardcode the correct path here, as we only have one path.
+
+        foreach (Category category in categories)
+        {
+            if (String.IsNullOrEmpty(request.Path))
+            {
+                Console.WriteLine("Path contains nothing OR nothing approved");
+                response.AddOrAppendToStatus("missing resource");
+                return response;
+            }
+            if (request.Path.Contains(partialPath))
+            {
+                //Run the ProcessValidatedRequests
+
+                Console.WriteLine("Valid path detected, continueing onwards to method ProcessValidatedRequests()");
+                ProcessValidatedRequests(request);
+                return response; //returning response after returning from Process ValidatedRequests
+            }
+            
+        }
+        return response;
+    }
+
+    
+    Response ProcessValidatedRequests(Request request)
+    {
+        Response response = new Response();
+
+        List<Category> categories = new List<Category>()
+        {
+        new Category(1,"Beverages"),
+        new Category(2, "Condiments"),
+        new Category(3, "Confections")
+        };
+
+        //partially valid path arrived.
+        const string partialPath = "/api/categories"; //We hardcode the correct path here, as we only have one path.
+        
+        IEnumerable<int> query = from Category category in categories
+                                 select category.cid;
+        foreach (int cid in query)
+        {
+            string testvar = partialPath + $"/{cid}";
+
+            if (request.Path == testvar)
+            {
+                Console.WriteLine($"Path {cid} was accessed");
+
+            }
+            else if (request.Path == partialPath)
+            {
+                Console.WriteLine("you accessed the whole categories table");
+            }
+
+            else
+            {
+                Console.WriteLine("Path does not exist");
+            }
+
+        }
+        //
+        
+        return response;
+    }
+
+
+
+    public static string ToJson(Response response)
     {
         return JsonSerializer.Serialize(response, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
     }
@@ -197,7 +329,19 @@ public class Server
         return JsonSerializer.Deserialize<Request>(element, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
     }
 
+     bool IsJason(string potentialJsonString)
+    {
+        try
+        {
+            JsonDocument.Parse(potentialJsonString);
+            
+            return true;
+        }
+        catch 
+        {
+            return false;
+        }
+    }
 
-   
 
 }
